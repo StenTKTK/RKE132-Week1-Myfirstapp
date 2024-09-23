e9b9f8283091d4d4aa8bb92e589104984c295cfa
Sten Eric Persidski <sten.eric.persidski@tktk.ee>
2024-09-23 16:28:54 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+
+//rakendus küsub kasutajalt sisestada tema nime ja seejärel rakendus tervitab kasutajat nime pidi.
+
+Console.WriteLine("Enter your name"); //Output
+//string on sõne
+string userName; //Input
+
+
+userName = Console.ReadLine();
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!"); //Output
